d013f051738b6a133f737f859c3eb098aa3e19c1
Павел Шевель <pawel.pavlenti@yandex.ru>
2022-11-19 17:55:13 +0300

    пермишены

---
+++ backend/foodgram/api/permissions.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+User = get_user_model()
+
+
+class IsAdmin(BasePermission):
+    """
+    Пользователь является супрюзером джанго
+    или имеет роль администратора.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin
+        )
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Пользователь является супрюзером джанго
+    или имеет роль администратора.
+    Просмотр доступен всем пользователям.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or (request.user.is_authenticated
+                and request.user.is_admin)
+        )
+
+
+class IsAuthorModeratorAdminOrReadOnly(BasePermission):
+    """
+    Пользователь является супрюзером джанго
+    или имеет роль администратора или модератора.
+    Просмотр доступен всем пользователям.
+    """
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+        )
+
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
